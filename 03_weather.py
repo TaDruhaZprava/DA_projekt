66af3d8d0aad0a63c13b9f693bcea9ec133800dc
TaDruhaZprava <pity.pity.pa@gmail.com>
2024-05-13 14:39:06 +0200

    Changed all the dates to the same datetime format and data type

--- 03_weather.py
+++ 03_weather.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 with open('data/03_weather.csv', mode='r', encoding='Windows 1252') as file: # nepouzivam encodig utf-8, protoze data jsou stazena v jinem.
     text = file.read()
@@ -7,3 +7,9 @@ with open('data/03_weather.csv', mode='r', encoding='Windows 1252') as file: # n
 
 with open('data/weather.csv', mode='w', encoding='utf-8') as output_file:
     print(weather, file=output_file)
+    
+# Changing the time to type datetime as in all the other tables
+weather = pd.read_csv('data/weather.csv')
+weather["time"] = pd.to_datetime(weather["time"], errors='coerce')
+weather.to_csv('data/weather_datetime.csv')
+ 
